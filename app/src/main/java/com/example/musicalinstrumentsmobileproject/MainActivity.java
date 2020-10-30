fa7f342cb8173cec97c5867d8c0139c5886c715e
Adam Baldwin <adam.baldwin@mycit.ie>
2020-10-30 18:01:34 +0000

    Made recyclerview with cards on main activity show list of instruments and their names.

--- app/src/main/java/com/example/musicalinstrumentsmobileproject/MainActivity.java
+++ app/src/main/java/com/example/musicalinstrumentsmobileproject/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.musicalinstrumentsmobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
+    String s1[];
+    int images[] = {R.drawable.accordion, R.drawable.acousticguitar, R.drawable.banjo,
+            R.drawable.drums, R.drawable.elecguitar, R.drawable.flute, R.drawable.harp,
+            R.drawable.piano, R.drawable.sax, R.drawable.trumpet};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.instruments);
+
+        MyRecyclerViewAdapter myRecyclerViewAdapter = new MyRecyclerViewAdapter(this, s1, images);
+        recyclerView.setAdapter(myRecyclerViewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
